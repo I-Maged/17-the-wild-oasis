07ebf5484c33cb4fb8a06d35f7a8225204f335d9
I-Maged <magedhanafy@gmail.com>
2026-02-17 08:52:41 +0200

    routes and layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+
 import Dashboard from './pages/Dashboard'
+import Bookings from './pages/Bookings'
+import Cabins from './pages/Cabins'
+import Account from './pages/Account'
+import Login from './pages/Login'
+import PageNotFound from './pages/PageNotFound'
+import Settings from './pages/Settings'
+import Users from './pages/Users'
+
+import GlobalStyles from './styles/Globalstyle'
+import AppLayout from './ui/AppLayout'
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='dashboard' element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to='dashboard' />} />
+            <Route path='dashboard' element={<Dashboard />} />
+            <Route path='bookings' element={<Bookings />} />
+            <Route path='cabins' element={<Cabins />} />
+            <Route path='users' element={<Users />} />
+            <Route path='settings' element={<Settings />} />
+            <Route path='account' element={<Account />} />
+          </Route>
+          <Route path='login' element={<Login />} />
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
