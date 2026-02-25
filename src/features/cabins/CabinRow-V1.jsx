1e679758f6128adafc3bfc261ccce5825a6226c1
I-Maged <magedhanafy@gmail.com>
2026-02-25 14:00:25 +0200

    compound menu

--- src/features/cabins/CabinRow-V1.jsx
+++ src/features/cabins/CabinRow-V1.jsx
@@ -7,7 +7,6 @@ import { useCreateCabin } from './useCreateCabin'
 import Modal from '../../ui/Modal'
 import ConfirmDelete from '../../ui/ConfirmDelete'
 import Table from '../../ui/Table'
-import Menus from '../../ui/Menus'
 
 // const TableRow = styled.div`
 //   display: grid;
@@ -51,7 +50,7 @@ const Discount = styled.div`
 const CabinRow = ({ cabin }) => {
   const { isPending, deleteCabin } = useDeleteCabin()
 
-  const { createCabin } = useCreateCabin()
+  const { isCreating, createCabin } = useCreateCabin()
 
   const {
     id: cabinId,
@@ -86,27 +85,25 @@ const CabinRow = ({ cabin }) => {
         <span>&mdash;</span>
       )}
       <div>
+        <button disabled={isCreating} onClick={handleDuplicate}>
+          <HiSquare2Stack />
+        </button>
+
         <Modal>
-          <Menus.Menu>
-            <Menus.Toggle id={cabinId} />
-
-            <Menus.List id={cabinId}>
-              <Menus.Button icon={<HiSquare2Stack />} onClick={handleDuplicate}>
-                Duplicate
-              </Menus.Button>
-
-              <Modal.Open opens='edit'>
-                <Menus.Button icon={<HiPencil />}>Edit</Menus.Button>
-              </Modal.Open>
-
-              <Modal.Open opens='delete'>
-                <Menus.Button icon={<HiTrash />}>Delete</Menus.Button>
-              </Modal.Open>
-            </Menus.List>
-          </Menus.Menu>
+          <Modal.Open opens='edit'>
+            <button>
+              <HiPencil />
+            </button>
+          </Modal.Open>
           <Modal.Window name='edit'>
             <CreateCabinForm cabinToEdit={cabin} />
           </Modal.Window>
+
+          <Modal.Open opens='delete'>
+            <button disabled={isPending} onClick={() => deleteCabin(cabinId)}>
+              <HiTrash />
+            </button>
+          </Modal.Open>
           <Modal.Window name='delete'>
             <ConfirmDelete
               resourceName={name}
