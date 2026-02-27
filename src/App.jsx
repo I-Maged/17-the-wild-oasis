843be025abf35abfc3ebe43b91d51bea2bded6f8
I-Maged <magedhanafy@gmail.com>
2026-02-28 00:47:03 +0200

    display single booking

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Login from './pages/Login'
 import PageNotFound from './pages/PageNotFound'
 import Settings from './pages/Settings'
 import Users from './pages/Users'
+import Booking from './pages/Booking'
 
 import GlobalStyles from './styles/Globalstyle'
 import AppLayout from './ui/AppLayout'
@@ -34,6 +35,7 @@ const App = () => {
             <Route index element={<Navigate replace to='dashboard' />} />
             <Route path='dashboard' element={<Dashboard />} />
             <Route path='bookings' element={<Bookings />} />
+            <Route path='bookings/:bookingId' element={<Booking />} />
             <Route path='cabins' element={<Cabins />} />
             <Route path='users' element={<Users />} />
             <Route path='settings' element={<Settings />} />
