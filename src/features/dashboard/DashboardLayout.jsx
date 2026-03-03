ea1e9e1fa806c28279a11173d0b18676d69ecb9e
I-Maged <magedhanafy@gmail.com>
2026-03-03 20:59:45 +0200

    get stats

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,8 +1,33 @@
-import styled from "styled-components";
+import styled from 'styled-components'
+import Spinner from '../../ui/Spinner'
+import { useRecentBookings } from './useRecentBookings'
+import { useRecentStays } from './useRecentStays'
 
 const StyledDashboardLayout = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
-`;
+`
+
+const DashboardLayout = () => {
+  const { isPending: isPendingBookings, bookings } = useRecentBookings()
+  const { isPending: isPendingStays, stays, confirmedStays } = useRecentStays()
+
+  if (isPendingBookings || isPendingStays) return <Spinner />
+
+  console.log(bookings)
+  console.log(stays)
+  console.log(confirmedStays)
+
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>Today's activity</div>
+      <div>Chart stay duration</div>
+      <div>Chart sales</div>
+    </StyledDashboardLayout>
+  )
+}
+
+export default DashboardLayout
